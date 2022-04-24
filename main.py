f0ed4d406956b8ac1ef0e461f2fc5a8dffb28961
Ian <itmacleod@email.wm.edu>
2022-04-24 17:36:10 -0400

    Asked user for number of letters, symbols, and numbers for their password

--- main.py
+++ main.py
@@ -7,3 +7,25 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
 symbols = ["!", "@", "#", "$", "%", "&", "*"]
 
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+password = ""
+
+numOfLetters = int(input("How many letters would you like: "))
+
+for i in range(0, numOfLetters):
+    val = random.choice(letters)
+    password += val
+
+numOfSymbols = int(input("How many symbols would you like: "))
+
+for i in range(0, numOfSymbols):
+    val = random.choice(symbols)
+    password += val
+
+numOfNumbers = int(input("How many numbers would you like: "))
+
+for i in range(0, numOfNumbers):
+    val = random.choice(numbers)
+    password += val
+
+print("Password: {}".format(password))
